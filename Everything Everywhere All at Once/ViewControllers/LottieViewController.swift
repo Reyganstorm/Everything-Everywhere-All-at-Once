6039adb20f8e91b6bcd83df1cc2145505d5ee495
Ruslan Shtybaev <79254522+Reyganstorm@users.noreply.github.com>
2023-06-15 21:00:36 +0300

    SETUP: - switcher lottie

--- Everything Everywhere All at Once/ViewControllers/LottieViewController.swift
+++ Everything Everywhere All at Once/ViewControllers/LottieViewController.swift
@@ -6,13 +6,37 @@
 //
 
 import UIKit
+import Lottie
 
 class LottieViewController: UIViewController {
+    
+    var switcherValue = false
+    
+    private let dayNigthSwitch: LottieAnimationView = {
+        let switcher = LottieAnimationView(name: "dayNightSwitch")
+        return switcher
+    }()
+    
+    private let switcherButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
         dissmisWithSwipe()
+        dayNigthSwitch.center = view.center
+        switcherButton.frame = dayNigthSwitch.frame
+        view.addSubview(dayNigthSwitch)
+        view.addSubview(switcherButton)
+        switcherButton.addTarget(self, action: #selector(switcherTogle), for: .touchUpInside)
     }
     
+    @objc private func switcherTogle() {
+        switch switcherValue {
+        case true:
+            dayNigthSwitch.play(fromProgress: 0.48, toProgress: 1)
+        case false:
+            dayNigthSwitch.play(fromProgress: 0, toProgress: 0.48)
+        }
+        switcherValue.toggle()
+    }
 }
